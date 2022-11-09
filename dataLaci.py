f39ca1dc45cb521c91a8e61278fba0e1e341a87a
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2022-11-09 10:50:03 +0100

    #9 valami baj van he

---
+++ dataLaci.py
@@ -0,0 +1,13 @@
+from functionsLaci import *
+f = open('')
+
+class Rednit:
+    
+    def __init__(self, row: str):
+        data = row.split(';')
+        self.name = data[0]
+        self.age = int(data[1])
+        self.gender = data[2]
+        self.residence = data[3]
+        self.children = int(data[4])
+        self.sexuality = data[5]
