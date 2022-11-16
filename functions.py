f365d836f0bb03988624e963c108957d5c8b3ce6
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2022-11-16 11:10:54 +0100

    #6 sok minden, lehet menüvel kell majd

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from data import Rednit
-
+from menu import menuMod
 people = []
+# choosenperson = []
 
 def readFile():
     f = open('Munkafuzet.csv', 'r', encoding='UTF-8')
@@ -19,15 +20,12 @@ def writeFile():
     
 
 def whichPerson():
-        name = input('Keresés: ')
-        index = 0
-        for i in people:
-            if name.lower() != people[i].lower():
-                index += 1
+    name = input('Kit keresünk? ')
+    for r in people:
+        if name.lower() in r.name.lower():
+            choosenperson = r.name.lower()
 
-        print(f'Kiválasztásra került: {people[index]}')
-        
-        return people[index]
+    return choosenperson
 
 
 def newPerson():
@@ -38,31 +36,24 @@ def newPerson():
     children = int(input('Utódok száma: '))
     sexuality = input('Vonzalom: ')
     row = f'{name};{age};{gender};{residence};{children};{sexuality}\n'
-    f = open('Munkafüzet1.csv', 'a', encoding = 'UTF-8')
+    f = open('Munkafuzet.csv', 'a', encoding = 'UTF-8')
     f.write(row)
     f.close
     r = Rednit(row)
     people.append(r)
 
 def modifyPerson():
-    name = input('Név: ')
-    for p in people:
-        if p.name.lower() == name.lower():
-            p.age = int(input('Kor: '))
-            p.gender = input('Nem: ')
-            p.residence = input('Százalék: ')
-            p.children = int(input('Utódok száma: '))
-            p.sexuality = input('Vonzalom: ')
-            writeFile()
-            return
-    input('Ilyen nevű felhasználó nincs.')
+    whichPerson()
+    choice = int(input('Mit akarunk változtatni?\n1= név\n2= kor\n3= nem\n4= lakhely\n5= utódok\n6= vonzalom:  '))
+    for r in people:
+        
 
 def detailsPerson():
     name = input('Név:  ')
     for r in people:
         if name.lower() in r.name.lower():
             print(f'{r.name}, {r.age} éves {r.gender}. Lakhelye {r.residence}, {r.children} utódja van. Szexualitása {r.sexuality}. ')
-    
+
     input('')
 
 
