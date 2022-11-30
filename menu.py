0e8fcc6204f4b3619da1abafaddea6f23b7bf5be
Fazekas Botond <fazekas.botond@students.jedlik.eu>
2022-11-30 10:25:27 +0100

    # 22

--- menu.py
+++ menu.py
@@ -1,13 +1,16 @@
 import os
 import sys
 from functions import *
+from colorama import init
+from termcolor import colored
 
 def menu():
-    print('1...Admin felület')
-    print('2...Normál felhasználói felület')
-    print('3...Kilépés a programból')
+    init()
+    print(colored('1...Admin felület' ,'magenta', 'on_white'))
+    print(colored('2...Normál felhasználói felület','white', 'on_magenta'))
+    print(colored('3...Kilépés a programból','magenta', 'on_white'))
  
-    choice=input('\nVálasztás (1..3): ')
+    choice=input(colored('\nVálasztás (1..3): ','white', 'on_magenta'))
     while len(choice)!=1 or choice<'1' or choice>'3':
         choice=input('\nVálasztás (0..3): ')
 
